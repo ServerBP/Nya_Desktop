58c2a0a5b29b343a652ea954053c5ba44b961df3
ServerBP <69462155+ServerBP@users.noreply.github.com>
2023-06-28 19:25:52 +0200

    Add files via upload

---
+++ popup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
+namespace Nya_Desktop
+{
+    public partial class popup : Form
+    {
+        public popup()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = true;
+        }
+
+        private void popup_Load(object sender, EventArgs e)
+        {
+            notification_lb.Text = category.notif;
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
